588a46e4183081cefdc0c11e18c05f23eb349974
Piotr Dulikowski <piodul@scylladb.com>
2020-10-15 13:49:15 +0200

    probably fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"flag"
 	"fmt"
@@ -550,7 +549,7 @@ func processStreams(stop <-chan struct{}, canAdvance <-chan struct{}, session *g
 				batchRowCount++
 
 				currentStats.RowsRead++
-				if bytes.Compare(lastTimestamp[:], timestamp[:]) < 0 {
+				if compareTimeuuid(lastTimestamp, timestamp) < 0 {
 					lastTimestamp = timestamp
 				}
 
@@ -585,6 +584,8 @@ func processStreams(stop <-chan struct{}, canAdvance <-chan struct{}, session *g
 				currentStats.IdlePolls++
 			}
 
+			fmt.Printf("Got %d rows\n", rowCount)
+
 			now := time.Now()
 			if now.After(nextReportTime) {
 				currentStats.TimeElapsed = now.Sub(processStartTime)
@@ -598,4 +599,25 @@ func processStreams(stop <-chan struct{}, canAdvance <-chan struct{}, session *g
 	}()
 
 	return statsChan
+}
+
+func compareTimeuuid(u1 gocql.UUID, u2 gocql.UUID) int {
+	// Compare timestamps
+	t1 := u1.Timestamp()
+	t2 := u2.Timestamp()
+	if t1 < t2 {
+		return -1
+	}
+	if t1 > t2 {
+		return 1
+	}
+
+	// Lexicographically compare the second half as signed bytes
+	for i := 8; i < 16; i++ {
+		d := int8(u1[i]) - int8(u2[i])
+		if d != 0 {
+			return int(d)
+		}
+	}
+	return 0
 }
